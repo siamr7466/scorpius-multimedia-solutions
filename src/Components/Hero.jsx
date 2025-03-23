78fee430abe189cbd364f73666708a07758315e8
siamr7466 <siamrofficial76@gmail.com>
2025-03-24 01:09:10 +0600

    Updated portfolio page design

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,8 +14,7 @@ const Hero = () => {
 
         {/* Subheading */}
         <h4 className="text-lg sm:text-xl md:text-xl lg:text-xl w-full md:w-1/2 lg:w-1/2 text-[#707070]">
-          From vision to reality, We design, edit and optimize, Your all-in-one
-          digital partner
+          From vision to reality, We design, edit and optimize, Your all-in-one digital partner
         </h4>
       </div>
     </div>
